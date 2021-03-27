258d1a1548aca8119c823863c4fc72498ad77b99
Sravan Chiittupalli <sravanchittupalli7@gmail.com>
2021-03-27 17:16:30 +0530

    added regex

--- CRAFT-pytorch/invoice_get_bb.py
+++ CRAFT-pytorch/invoice_get_bb.py
@@ -8,6 +8,8 @@ from collections import OrderedDict
 import cv2
 import numpy as np
 
+import re
+
 # PyTorch Imports
 import torch
 import torch.backends.cudnn as cudnn
@@ -25,6 +27,146 @@ from craft import CRAFT
 
 #tesseract config
 custom_config = r'--psm 11'
+
+receipt_ocr = {}
+
+def get_date(extracted_text):
+# regex for date. The pattern in the receipt is in 30.07.2007 in DD:MM:YYYY
+    date_pattern = r'(0[1-9]|[12][0-9]|3[01]|0[1-9]|1[012])[-./](0[1-9]|[12][0-9]|3[01]|0[1-9]|1[012])[-./](20[012][0-9]|[0-3][0-9])'
+    pattern = re.compile(date_pattern)
+    matches = pattern.finditer(extracted_text)
+    date = None
+    for match in matches:
+        date = extracted_text[match.span()[0]:match.span()[1]]
+    receipt_ocr['date'] = date
+    print(receipt_ocr)
+
+def get_time(extracted_text):
+# regex for time. The pattern in the receipt is in 8:47:20 
+    time_pattern = r'([0-9]|0[0-9]|[1][0-9]|2[0-4])[:]([0-5][0-9])[:]([0-5][0-9])*'
+    pattern = re.compile(time_pattern)
+    matches = pattern.finditer(extracted_text)
+    time = None
+    for match in matches:
+        time = extracted_text[match.span()[0]:match.span()[1]]
+    receipt_ocr['time'] = time
+    print(receipt_ocr)
+
+def get_total(extracted_text):
+    total_pattern = r'[tTiI][oOaA][tTlLiL][oOaA0cC]'
+    # total_pattern = r'Total'
+    splits = extracted_text.split('\n')
+    lines_with_total = []
+    for line in splits:
+        if re.search(total_pattern, line):
+            lines_with_total.append(line)
+    
+    amount = []
+    amount_pattern = r'[0-9]+\.[0-9]+'
+    pattern = re.compile(amount_pattern)
+    for line in lines_with_total:
+        matches = pattern.finditer(line)
+        for match in matches:
+            amount.append(float(line[match.span()[0]:match.span()[1]]))
+
+    receipt_ocr['total'] = str(max(amount))
+    print(receipt_ocr)
+
+def get_company_name(extracted_text):
+    splits = extracted_text.splitlines()
+    i = 0
+    pattern = r'([0-9]+|[-./]+|\\)'
+
+    for split in splits:
+        if split==' ':
+            # print(split)
+            i+=1
+            continue
+        if re.search(pattern, split):
+            # print(split)
+            i+=1
+        else:
+            break
+    restaurant_name = splits[i] + '\n' + splits[i+2]
+    receipt_ocr['Comapny Name'] = restaurant_name
+    print(receipt_ocr)
+
+def get_GST_number(extracted_text):
+    pattern = r'(GST)'
+    splits = extracted_text.split('\n')
+    lines_with_GST = []
+    for line in splits:
+        if re.search(pattern, line, re.IGNORECASE):
+            lines_with_GST.append(line)
+
+    gst_num = ''
+    gst_num_pattern = r'[0-9]{3,15}\s*[0-9]*\s*[0-9]*'
+    pattern = re.compile(gst_num_pattern)
+    for line in lines_with_GST:
+        matches = pattern.finditer(line)
+        for match in matches:
+            gst_num_curr = line[match.span()[0]:match.span()[1]]
+            if len(gst_num_curr) > len(gst_num):
+                gst_num = gst_num_curr
+
+    receipt_ocr['GST Number'] = gst_num
+    print(receipt_ocr)
+
+def get_email(extracted_text):
+    pattern = (r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+')
+    pattern = re.compile(pattern)
+    matches = pattern.finditer(extracted_text)
+    email = None
+    for match in matches:
+        email = extracted_text[match.span()[0]:match.span()[1]]
+    receipt_ocr['email'] = email
+    print(receipt_ocr)
+
+
+def get_phone_number(extracted_text):
+    pattern = (r'(Telephone|tel|Ph|Phone|Mob|Mobile|dial)')
+    splits = extracted_text.split('\n')
+    lines_with_phone_num = []
+    for line in splits:
+        if re.search(pattern, line, re.IGNORECASE):
+            lines_with_phone_num.append(line)
+
+    phone_num = []
+
+    phone_num_pattern = r'[+-]*[0-9]+[-]*[0-9]*\s*[+-]*[0-9]*[-.)(]*[0-9]*\s*'
+    pattern = re.compile(phone_num_pattern)
+    for line in lines_with_phone_num:
+        matches = pattern.finditer(line)
+        for match in matches:
+            phone_num.append(line[match.span()[0]:match.span()[1]])
+
+    receipt_ocr['Phone Number'] = phone_num
+    print(receipt_ocr)
+
+def get_invoice_number(extracted_text):
+    pattern = (r'(invoice|bill|inv|inc|doc|document|order|ord|receipt|billing|token|trn|transaction|trx|tally|statement)')
+    splits = extracted_text.split('\n')
+    lines_with_invoice_num = []
+    for line in splits:
+        if re.search(pattern, line, re.IGNORECASE):
+            lines_with_invoice_num.append(line)
+
+    invoice_num = []
+    final_invoice = None
+    invoice_num_pattern = r'[a-zA-Z]*[0-9]+[/:-]*[0-9]*[/:-]*[0-9]*'
+    pattern = re.compile(invoice_num_pattern)
+    for line in lines_with_invoice_num:
+        matches = pattern.finditer(line)
+        for match in matches:
+            invoice_num.append(line[match.span()[0]:match.span()[1]])
+
+    for invoice in invoice_num:
+        if '/' not in invoice and ':' not in invoice and ' ' not in invoice:
+            final_invoice = invoice
+
+    receipt_ocr['Invoice Number'] = final_invoice
+    print(receipt_ocr)
+
 
 def copyStateDict(state_dict):
     if list(state_dict.keys())[0].startswith("module"):
@@ -55,7 +197,7 @@ args = parser.parse_args()
 
 # Store results here
 result_folder = './result/'
-image_path = '/home/sravanchittupalli/konnoha/clones/Invoice-Processing-System/tesseract/assets/input/X00016469619.jpg'
+# image_path = '/home/sravanchittupalli/konnoha/clones/Invoice-Processing-System/tesseract/assets/input/X00016469619.jpg'
 if not os.path.isdir(result_folder):
     os.mkdir(result_folder)
 
@@ -166,17 +308,32 @@ def main(image):
             max_line = 0
     print(len(lines))
 
+    text_in_img = ''
     image_line = image.copy()
     for line in lines:
         cv2.rectangle(image_line, (0, line[0]), (image.shape[1], line[1]), (90,239,155), 3)
         roi = image[line[0]-3:line[1]+3, 0:image.shape[1], :]
         img = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
         extracted_text = pytesseract.image_to_string(img, config=custom_config)
-        print(extracted_text)
-        cv2.imshow('line_img', img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        # print(extracted_text)
+        line = ''
+        for extract in extracted_text.split('\n'):
+            line = line+extract+' '
+        text_in_img = text_in_img+line+'\n'
+        # cv2.imshow('line_img', img)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
 
+    print(text_in_img)
+
+    get_date(text_in_img)
+    get_time(text_in_img)
+    get_total(text_in_img)
+    get_company_name(text_in_img)
+    get_GST_number(text_in_img)
+    get_email(text_in_img)
+    get_phone_number(text_in_img)
+    get_invoice_number(text_in_img)
     cv2.imshow('line_img', image_line)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -194,5 +351,6 @@ def main(image):
 
 
 if __name__ == '__main__':
-    image = cv2.imread('/home/sravanchittupalli/konnoha/clones/Invoice-Processing-System/tesseract/assets/input/X51005301661.jpg')
+    image_path = '/home/sravanchittupalli/konnoha/clones/Invoice-Processing-System/CRAFT-pytorch/assets/X51005441402.jpg'
+    image = cv2.imread(image_path)
     main(image)
